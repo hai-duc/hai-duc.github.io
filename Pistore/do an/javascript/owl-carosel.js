7818ce7f4a87ffa1afcc1f7d4c276c9e62ccb0c9
hai-duc <vuhaiduc@gmail.com>
2020-05-18 18:38:54 +0700

    Pistore

---
+++ Pistore/do an/javascript/owl-carosel.js
@@ -0,0 +1,31 @@
+$('.owl-carousel').owlCarousel({
+    loop:true,
+    margin:10,
+    nav:true,
+    responsive:{
+        300:{
+            items:1.5
+        },
+        400:{
+            items:1.85
+        },
+        500:{
+            items:2.15
+        },
+        600:{
+            items:2.25
+        },
+        700:{
+            items:2.5
+        },
+        800:{
+            items:3
+        },
+        900:{
+            items:3.25
+        },
+        1000:{
+            items:3.65
+        }
+    }
+})
