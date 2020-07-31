21f2a4bdb4f252eea4f962ad6333519fe107eadb
hai-duc <vuhaiduc@gmail.com>
2020-07-31 12:14:17 +0700

    render html bang reactJS

--- react-shopping-cart-v2/footer.js
+++ react-shopping-cart-v2/footer.js
@@ -1,4 +1,10 @@
-const Footer = () => {
+const Footer = ({ products }) => {
+  let totalItem = 0;
+  products.map((product) => {
+   totalItem += product.quantity * product.price;
+
+    return <div key={products.id}>{totalItem}</div>;
+  });
   return (
     <section className="container">
       <div className="promotion">
@@ -8,13 +14,13 @@ const Footer = () => {
       <div className="summary">
         <ul>
           <li>
-            Subtotal <span>$21.97</span>
+            Subtotal <span>${totalItem}</span>
           </li>
           <li>
             Tax <span>$5.00</span>
           </li>
           <li className="total">
-            Total <span>$26.97</span>
+            Total <span>${totalItem +5}</span>
           </li>
         </ul>
       </div>
