d2e2fafde4e5977d177845ce7732d00d29334d20
hai-duc <vuhaiduc@gmail.com>
2020-10-13 23:50:35 +0700

    cat html techmaster

---
+++ test-techmaster/script.js
@@ -0,0 +1,25 @@
+// owlCarousel
+$('.owl-carousel').owlCarousel({
+    loop:true,
+    margin:10,
+    nav:false,
+    responsive:{
+        0:{
+            items:1
+        },
+        600:{
+            items:3
+        },
+        1000:{
+            items:2.5
+        },
+        1600:{
+            items:2.5
+        }
+    }
+})
+
+// navbar scroll
+// jQuery code
+// Prevent closing from click inside dropdown
+
