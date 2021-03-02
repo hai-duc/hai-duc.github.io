2b46de658cec77ab0465f28cdd1e99d8008d8c91
hai-duc <vuhaiduc@gmail.com>
2021-03-02 08:58:04 +0700

    ok

---
+++ js(garelly image)/script.js
@@ -0,0 +1,33 @@
+function chooseItem() {
+  document.querySelector(".parentItem").style.display = "none";
+  document.querySelector(".slideshow").style.display = "block";
+}
+
+
+
+
+var slideIndex = 1;
+showSlides(slideIndex);
+
+function plusSlides(n) {
+  showSlides(slideIndex += n);
+}
+
+function currentSlide(n) {
+  showSlides(slideIndex = n);
+}
+
+function showSlides(n) {
+  var i;
+  var slides = document.getElementsByClassName("mySlides");
+  var dots = document.getElementsByClassName("demo");
+  var captionText = document.getElementById("caption");
+  if (n > slides.length) {slideIndex = 1}
+  if (n < 1) {slideIndex = slides.length}
+  for (i = 0; i < slides.length; i++) {
+      slides[i].style.display = "none";
+  }
+  
+  slides[slideIndex-1].style.display = "block";
+  
+}
